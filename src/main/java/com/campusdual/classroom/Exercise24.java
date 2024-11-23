f4d6c3698d8685223684c980c0f2ca891d0409d8
Gabriel <gabriel.lamela.ch@gmail.com>
2024-11-23 13:10:05 +0100

    Ejercicio 24 resuelto

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -5,14 +5,23 @@ import java.util.Queue;
 
 public class Exercise24 {
     public static Queue<String> createQueue() {
-
+        Queue<String> cola = new LinkedList<>();
+        cola.offer("Smith");
+        cola.offer("Montessori");
+        cola.offer("Peralta");
+        cola.offer("House");
+        return cola;
     }
     public static void printAndEmptyQueue(Queue<String> queue) {
-
+        while (!queue.isEmpty()){
+            System.out.println(queue.poll());
+        }
     }
 
     public static void main(String[] args) {
+        Queue<String> cola = createQueue();
 
+        printAndEmptyQueue(cola);
     }
 
 }
